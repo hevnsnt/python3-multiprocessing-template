7c937df5743f8418a57674919d4000cd687231c4
hevnsnt <hevnsnt@gmail.com>
2020-05-22 23:10:40 -0500

    Update multiproc.py

--- multiproc.py
+++ multiproc.py
@@ -1,4 +1,4 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 import argparse
 from time import sleep
 
@@ -12,27 +12,40 @@ def readfile(file):
         pass
 
 def work(line):
-        print(f"going to du some work on {line}")
+        print(f"going to do some work on {line}")
         countdown(2)
 
 def countdown(time=30):
   #defaults to 30secs unless you pass it a different value
-  for remaining in range(time, 0, -1):
+  sleep(3)
+  '''for remaining in range(time, 0, -1):
     print(f"\r<<<<  {remaining} seconds remain before auto-resume >>>>", end=" ", flush=True)
-    sleep(1)
+    sleep(1)'''
+
+def parseArgs(args):
+  if args.verbose:
+    verbose = True
+    print("[+] Verbosity turned on")
+  else:
+    verbose = False
+  if args.threads:
+    threads = args.threads
+  else:
+    threads = cpu_count()
+  print(f'[+] Using {threads} threads')
+  return threads, verbose, args.file
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument("-f", "--file", help="Insert the flie you plan on parsing")
+  parser.add_argument("-f", "--file", required = True, help="Insert the flie you plan on parsing")
   parser.add_argument("-t", "--threads", help="Number of threads, by default will use all available processors")
   parser.add_argument("-v", "--verbose", help="increase output verbosity",
                        action="store_true")
-  parser.parse_args()
-  if args.verbose:
-    print("verbosity turned on")
+  threads, verbose, filename = parseArgs(parser.parse_args())
   
-  pool = Pool(4) # Number of threads going to use
-  pool.map(work, urls)
+  pool = Pool(threads) # Number of threads going to use
+  pool.map(work)
+  readfile(filename)
   pool.close()
   pool.join()
